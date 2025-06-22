16e14c3851a025befd58783e05c6dd04b1e06f39
satin <satin3737@gmail.com>
2025-06-22 17:05:46 +0300

    configure esbuild to load environment variables from dotenv and update entry points for EJS files

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,17 +1,20 @@
+import dotenv from 'dotenv';
 import esbuild from 'esbuild';
 import {globSync} from 'glob';
 
-const ejsFiles = globSync('src/views/**/*.ejs');
+dotenv.config();
 
 esbuild
     .build({
-        entryPoints: ['src/app.ts', ...ejsFiles],
+        entryPoints: ['src/app.ts', ...globSync('src/views/**/*.ejs')],
         bundle: true,
         minify: true,
         sourcemap: true,
         platform: 'node',
         outdir: 'dist',
         loader: {'.ejs': 'copy'},
-        external: ['pg-hstore']
+        define: Object.fromEntries(
+            Object.entries(process.env).map(([key, value]) => [`process.env.${key}`, JSON.stringify(value)])
+        )
     })
     .catch(() => process.exit(1));
